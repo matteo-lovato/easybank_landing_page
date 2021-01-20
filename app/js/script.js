4ba45762dd1436eea032ec31bb8dd43f54b784f6
matteo-lovato <lovato.teo@gmail.com>
2021-01-20 19:05:03 +0100

    fade in fade out burger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,11 @@
 btnHamburger.addEventListener("click", () => {
     console.log("clicked");
-    btnHamburger.classList.toggle("open");
+    header.classList.toggle("open");
+    if (header.classList.contains("open")) {
+        overlay.classList.remove("fade-out");
+        overlay.classList.add("fade-in");
+    } else {
+        overlay.classList.add("fade-out");
+        overlay.classList.remove("fade-in");
+    }
 });
